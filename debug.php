c6ebad8c3b6a29aab0cc6834ed70de2d96c2b7f3
Pablo Zambrano <pablojz70@gmail.com>
2026-05-25 15:46:37 -0400

    Elimina debug.php

--- debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? 'NOT SET') . "\n";
-echo "REQUEST_URI: " . ($_SERVER['REQUEST_URI'] ?? 'NOT SET') . "\n";
-echo "ORIG_SCRIPT_NAME: " . ($_SERVER['ORIG_SCRIPT_NAME'] ?? 'NOT SET') . "\n";
-echo "PHP_SELF: " . ($_SERVER['PHP_SELF'] ?? 'NOT SET') . "\n";
-echo "REDIRECT_URL: " . ($_SERVER['REDIRECT_URL'] ?? 'NOT SET') . "\n";
-echo "REDIRECT_SCRIPT_NAME: " . ($_SERVER['REDIRECT_SCRIPT_NAME'] ?? 'NOT SET') . "\n";
-?>
